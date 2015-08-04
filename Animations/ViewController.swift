ad747ae1cd6e906c02e14d8475b84d0f5145bb1a
Hirad Motamed <hirad@pendarlabs.com>
2015-08-04 09:23:55 -0700

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Hirad Motamed on 2015-08-04.
+//  Copyright (c) 2015 Pendar Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
